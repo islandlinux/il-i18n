1b60a95886ee673f98c658b6de34edb7ec413259
islandlinux <dvogels@islandlinux.org>
2016-03-17 19:05:17 -0700

    #1 initialized environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -80,10 +80,10 @@ module.exports = function (grunt)
             // The actual grunt server settings
             connect: {
                 options: {
-                    port: 9000,
+                    port: 9001,
                     // Change this to '0.0.0.0' to access the server from outside.
                     hostname: 'localhost',
-                    livereload: 35729
+                    livereload: 35728
                 },
                 livereload: {
                     options: {
@@ -102,7 +102,7 @@ module.exports = function (grunt)
                 },
                 test: {
                     options: {
-                        port: 9001,
+                        port: 9002,
                         middleware: function (connect)
                         {
                             return [
